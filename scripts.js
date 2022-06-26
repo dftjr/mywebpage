e38052f535edecf2f5bab24eef271054dddfedf0
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-06-26 08:20:24 -0700

    Added JavaScript

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function WelcomeCoder() {
+  confirm("Welcome fellow coder!");
+}
+
+function VisitorBook() {
+  let person = prompt("Please add your nameto the website log", "Enter Name here!");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Welcome " + person + " Enjoy your visit!";
+  }
+}
